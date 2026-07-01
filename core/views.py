1cff1bd5522d5615b2222e176b276b2c43046b04
prabeshbuilds <prabeshuser407@gmail.com>
2026-07-01 22:06:33 +0545

    added some file

--- core/views.py
+++ core/views.py
@@ -3,5 +3,6 @@ from django.http import JsonResponse
 
 def home(request):
     return JsonResponse({
-        "message": "Django CI/CD is running successfullyyyyyy!"
+        "message": "Django CI/CD is running successfullyyyyyy!",
+        "welcome": "Hello, welcome to the Django CI/CD pipeline."
     })
